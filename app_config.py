f64521074a4765d4f50599d0edafc3c502856070
Saurabh Kathpalia <saurabhkathpalia@yahoo.com>
2022-12-28 12:39:02 +0530

    Update app_config.py

--- app_config.py
+++ app_config.py
@@ -12,13 +12,10 @@ CLIENT_ID = os.getenv("CLIENT_ID")
 
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 
-AUTHORITY = authority_template.format(tenant=b2c_tenant,
-                                      user_flow=signupsignin_user_flow)
-B2C_PROFILE_AUTHORITY = authority_template.format(
-    tenant=b2c_tenant, user_flow=editprofile_user_flow)
+AUTHORITY = authority_template.format(tenant=b2c_tenant, user_flow=signupsignin_user_flow)
+B2C_PROFILE_AUTHORITY = authority_template.format(tenant=b2c_tenant, user_flow=editprofile_user_flow)
 
-B2C_RESET_PASSWORD_AUTHORITY = authority_template.format(
-    tenant=b2c_tenant, user_flow=resetpassword_user_flow)
+B2C_RESET_PASSWORD_AUTHORITY = authority_template.format(tenant=b2c_tenant, user_flow=resetpassword_user_flow)
 
 REDIRECT_PATH = "/getAToken"
 # This is the API resource endpoint

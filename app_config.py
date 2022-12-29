43dc5f6594ff8d431ed404d59b6ec62555a00c69
Saurabh Kathpalia <saurabhkathpalia@yahoo.com>
2022-12-29 10:23:23 +0530

    Update app_config.py

--- app_config.py
+++ app_config.py
@@ -20,7 +20,7 @@ B2C_RESET_PASSWORD_AUTHORITY = authority_template.format(tenant=b2c_tenant, user
 
 REDIRECT_PATH = "/getAToken"
 # This is the API resource endpoint
-ENDPOINT = 'https://mmt32.onmicrosoft.com'  #
+ENDPOINT = 'https://mmt32.onmicrosoft.com/tasks-api'  #
 SCOPE = [
     os.getenv("TASK_READ"),
     os.getenv("TASK_WRITE")
